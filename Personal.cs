bf84c4cb89b5978cec5ff0584f69f293427bd0b9
Noname136-rpg <zofhfh2323@gmail.com>
2023-11-14 16:15:33 +0900

    1차 보완
    필수 사항 - 아이템 장착시 플레이어 스탯에 반영되도록 추가
    선택 사항
    1. 인벤토리 크기 맞춤
    2. 상점 기능 (아이템 구매 / 판매)

--- Personal.cs
+++ Personal.cs
@@ -14,6 +14,7 @@ internal class Personal
         DisplayMyInfo,
         DisplayInventory,
         DisplayMarket,
+        DisplayMarketSell,
         DisplayDungeon,
         SaveScene,
         Error
@@ -31,6 +32,11 @@ internal class Personal
         Player.InventroyTest(gm.item_list[3]);
 
         //Console.ReadLine();
+
+        //줄맞추기는 패딩이 필요함
+        //stirng을 출력할때 .LPAD RPAD 한쪽에다 글을 채움
+        //문제는 콘솔창에서 한글이 2배로 인색된다.
+        //또한 폰트가 일정하지 않을 수 있다.
         while (true)
         {
             switch (sceneStat)
@@ -55,6 +61,11 @@ internal class Personal
                         DisplayMarket();
                         break;
                     }
+                case SceneStat.DisplayMarketSell:
+                    {
+                        DisplayMarketSell();
+                        break;
+                    }
                 case SceneStat.DisplayDungeon:
                     {
                         DisplayDungeon();
@@ -77,11 +88,13 @@ internal class Personal
     static void GameDataSetting()
     {
         // 캐릭터 정보 세팅
-        //player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
         gm.LoadPlayers();
 
         // 아이템 정보 세팅
         gm.LoadItems();
+
+        // 상점 정보 세팅 (gm의 아이템 리스트 copy)
+        gm.LoadMarket();
     }
 
     static void ChooseSave()
@@ -148,9 +161,23 @@ internal class Personal
         Console.WriteLine("캐릭터의 정보를 표시합니다.");
         Console.WriteLine();
         Console.WriteLine("Lv.{0}",Player.Level.ToString("D2"));
-        Console.WriteLine($"{Player.Name}({Player.Job})");
-        Console.WriteLine($"공격력 :{Player.Atk}");
-        Console.WriteLine($"방어력 : {Player.Def}");
+        Console.WriteLine($"{Player.Name} ({Player.Job})");
+        if((Player.PlayerAtk() - Player.Atk)==0)
+        {
+            Console.WriteLine($"공격력 :{Player.Atk}");
+        }
+        else
+        {
+            Console.WriteLine($"공격력 :{Player.Atk} (+{Player.PlayerAtk() - Player.Atk})");
+        }
+        if ((Player.PlayerDef() - Player.Def) == 0)
+        {
+            Console.WriteLine($"방어력 : {Player.Def}");
+        }
+        else
+        {
+            Console.WriteLine($"방어력 : {Player.Def} (+{Player.PlayerDef() - Player.Def})");
+        }
         Console.WriteLine($"체력 : {Player.Hp}");
         Console.WriteLine($"Gold : {Player.Gold} G");
         Console.WriteLine();
@@ -168,22 +195,28 @@ internal class Personal
     static void DisplayInventory()
     {
         Console.Clear();
-
+        //string str = " ";
         Console.WriteLine("인벤토리");
         Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
         Console.WriteLine("[아이템 목록]\n");
 
-        for(int inven_cnt=0; inven_cnt < Player.Inventory.Count; inven_cnt++)
+        if (Player.Inventory.Count == 0)
         {
-            if(Player.Inventory[inven_cnt].Equip == false)
+            Console.WriteLine("인벤토리가 비었습니다.");
+        }
+        else
+        {
+            for (int inven_cnt = 0; inven_cnt < Player.Inventory.Count; inven_cnt++)
             {
-                Console.WriteLine("{0}. {1} | 공격력 +{2} | 방어력 +{3} | {4}", inven_cnt+1, Player.Inventory[inven_cnt].Name, Player.Inventory[inven_cnt].Atk, Player.Inventory[inven_cnt].Def, Player.Inventory[inven_cnt].Annot);
+                if (Player.Inventory[inven_cnt].Equip == false)
+                {
+                    Console.WriteLine("{0}. {1} | 공격력 +{2} | 방어력 +{3} | {4}", inven_cnt + 1, Player.Inventory[inven_cnt].Name.PadRight(10, ' '), (Player.Inventory[inven_cnt].Atk).ToString().PadRight(3, ' '), (Player.Inventory[inven_cnt].Def).ToString().PadRight(3, ' '), (Player.Inventory[inven_cnt].Annot).ToString().PadRight(30, ' '));
+                }
+                else
+                {
+                    Console.WriteLine("[E] {0}. {1} | 공격력 +{2} | 방어력 +{3} | {4}", inven_cnt + 1, Player.Inventory[inven_cnt].Name.PadRight(10, ' '), (Player.Inventory[inven_cnt].Atk).ToString().PadRight(3, ' '), (Player.Inventory[inven_cnt].Def).ToString().PadRight(3, ' '), (Player.Inventory[inven_cnt].Annot).ToString().PadRight(30, ' '));
+                }
             }
-            else
-            {
-                Console.WriteLine("[E]{0}. {1} | 공격력 +{2} | 방어력 +{3} | {4}", inven_cnt+1, Player.Inventory[inven_cnt].Name, Player.Inventory[inven_cnt].Atk, Player.Inventory[inven_cnt].Def, Player.Inventory[inven_cnt].Annot);
-            }
-            
         }
 
         Console.WriteLine("\n1. 장착관리");
@@ -197,15 +230,23 @@ internal class Personal
                 sceneStat = SceneStat.DisplayGameIntro;
                 break;
             case 1:
-                Console.Write("장착 / 해제할 아이템을 입력해주세요\n>>");
-                int select_inventroy = CheckValidInput(1, Player.Inventory.Count);
-                if(Player.Inventory[select_inventroy-1].Equip == true)
+                if(Player.Inventory.Count==0)
                 {
-                    Player.Inventory[select_inventroy-1].Equip = false;
+                    Console.Write("장착 / 해제할 아이템이 없습니다.");
+                    Thread.Sleep(1000);
                 }
                 else
                 {
-                    Player.Inventory[select_inventroy-1].Equip = true;
+                    Console.Write("장착 / 해제할 아이템을 입력해주세요\n>>");
+                    int select_inventroy = CheckValidInput(1, Player.Inventory.Count);
+                    if (Player.Inventory[select_inventroy - 1].Equip == true)
+                    {
+                        Player.Inventory[select_inventroy - 1].Equip = false;
+                    }
+                    else
+                    {
+                        Player.Inventory[select_inventroy - 1].Equip = true;
+                    }
                 }
                 break;
         }
@@ -239,16 +280,104 @@ internal class Personal
 
         Console.WriteLine("상점");
         Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
+
+        Console.WriteLine("[보유 골드]");
+        Console.WriteLine($"{Player.Gold} \n");
+
+        Console.WriteLine("[아이템 목록]");
+        for (int market_cnt = 0; market_cnt < gm.Market_item.Count; market_cnt++)
+        {
+            if(gm.Market_item[market_cnt].Gold == -1)
+            {
+                Console.WriteLine($"- {market_cnt+1} {gm.Market_item[market_cnt].Name} | 공격력 +{gm.Market_item[market_cnt].Atk} | 방어력 +{gm.Market_item[market_cnt].Def} | {gm.Market_item[market_cnt].Annot} | 구매완료");
+            }
+            else
+            {
+                Console.WriteLine($"- {market_cnt+1} {gm.Market_item[market_cnt].Name} | 공격력 +{gm.Market_item[market_cnt].Atk} | 방어력 +{gm.Market_item[market_cnt].Def} | {gm.Market_item[market_cnt].Annot} | {gm.Market_item[market_cnt].Gold}G");
+            }
+        }
+        Console.WriteLine("\n1. 아이템 구매");
+        Console.WriteLine("2. 아이템 판매");
         Console.WriteLine("0. 나가기\n");
         Console.Write("원하시는 행동을 입력해주세요\n>>");
 
-        int input = CheckValidInput(0, 0);
+        int input = CheckValidInput(0, 2);
+        switch (input)
+        {
+            case 1:
+                Console.Write("구매하고 싶은 아이템을 입력해주세요\n>>");
+                int buy = CheckValidInput(1, gm.Market_item.Count);
+                if(Player.Gold < gm.Market_item[buy-1].Gold)
+                {
+                    Console.WriteLine("Gold가 부족합니다.");
+                    Thread.Sleep(400);
+                }
+                else if(gm.Market_item[buy-1].Gold == -1)
+                {
+                    Console.WriteLine("이미 판매된 아이템입니다.");
+                    Thread.Sleep(400);
+                }
+                else
+                {
+                    Player.BuyItem(gm.Market_item[buy - 1]);
+                    gm.Market_item[buy - 1].Gold = -1;
+                }
+                break;
+
+            case 2:
+                sceneStat = SceneStat.DisplayMarketSell;
+                break;
+
+            case 0:
+                sceneStat = SceneStat.DisplayGameIntro;
+                break;
+        }
+    }
+
+    static void DisplayMarketSell()
+    {
+        Console.Clear();
+
+        Console.WriteLine("상점 - 아이템 판매");
+        Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
+
+        Console.WriteLine("[보유 골드]");
+        Console.WriteLine($"{Player.Gold} \n");
+
+        if(Player.Inventory.Count==0)
+        {
+            Console.WriteLine("인벤토리가 비었습니다.");
+        }
+        else
+        {
+            for (int inven_cnt = 0; inven_cnt < Player.Inventory.Count; inven_cnt++)
+            {
+                if (Player.Inventory[inven_cnt].Equip == false)
+                {
+                    Console.WriteLine("{0}. {1} | 공격력 +{2} | 방어력 +{3} | {4}", inven_cnt + 1, Player.Inventory[inven_cnt].Name.PadRight(10, ' '), (Player.Inventory[inven_cnt].Atk).ToString().PadRight(3, ' '), (Player.Inventory[inven_cnt].Def).ToString().PadRight(3, ' '), (Player.Inventory[inven_cnt].Annot).ToString().PadRight(30, ' '));
+                }
+                else
+                {
+                    Console.WriteLine("[E] {0}. {1} | 공격력 +{2} | 방어력 +{3} | {4}", inven_cnt + 1, Player.Inventory[inven_cnt].Name.PadRight(10, ' '), (Player.Inventory[inven_cnt].Atk).ToString().PadRight(3, ' '), (Player.Inventory[inven_cnt].Def).ToString().PadRight(3, ' '), (Player.Inventory[inven_cnt].Annot).ToString().PadRight(30, ' '));
+                }
+            }
+        }
+
+        Console.WriteLine("\n0. 나가기\n");
+        Console.Write("판매하고 싶은 아이템을 입력해주세요\n>>");
+
+        int input = CheckValidInput(0, Player.Inventory.Count);
         switch (input)
         {
             case 0:
                 sceneStat = SceneStat.DisplayGameIntro;
                 break;
+            default:
+                Player.SellItem(input - 1);
+                gm.Market_item[input - 1].Gold = gm.item_list[input - 1].Gold;
+                break;
         }
+
     }
 
     static int CheckValidInput(int min, int max)
@@ -273,6 +402,7 @@ public class GameManager
 {
     public List<Item> item_list = new List<Item>();
     public List<Character> characters = new List<Character>();
+    public List<Item> Market_item = new List<Item>();
 
     public void LoadPlayers()
     {
@@ -351,6 +481,14 @@ public class GameManager
         sr.Close();
     }
 
+    public void LoadMarket()
+    {
+        foreach(Item item in item_list)
+        {
+            Item input_item = new Item(item.Name, item.Atk, item.Def, item.Annot, item.Gold);
+            Market_item.Add(input_item);
+        }
+    }
 }
 
 
@@ -361,7 +499,7 @@ public class Item
     public int Atk { get; }
     public int Def { get; }
     public string Annot { get; }
-    public int Gold { get; }
+    public int Gold { get; set; }
 
     public Item(string _name, int _atk, int _def, string _annot, int _gold)
     {
@@ -373,6 +511,7 @@ public class Item
         Gold = _gold;
     }
 }
+
 public class Character
 {
     public string Name { get; }
@@ -381,7 +520,7 @@ public class Character
     public int Atk { get; }
     public int Def { get; }
     public int Hp { get; }
-    public int Gold { get; }
+    public int Gold { get; set; }
 
     //상점 미구현으로 인해 임시로 아이템을 넣어 두겠습니다.
     public List<Item> Inventory = new List<Item>();
@@ -389,6 +528,49 @@ public class Character
     public void InventroyTest(Item item)
     {
         Inventory.Add(item);
+    }
+
+    public int PlayerAtk()
+    {
+        int totalAtk = Atk;
+
+        foreach(Item item in Inventory)
+        {
+            if (item.Equip == true)
+            {
+                totalAtk += item.Atk;
+            }
+        }
+
+        return totalAtk;
+    }
+
+    public int PlayerDef()
+    {
+        int totalDef = Def;
+
+        foreach (Item item in Inventory)
+        {
+            if(item.Equip==true)
+            {
+                totalDef += item.Def;
+            }
+        }
+
+        return totalDef;
+    }
+
+    public void BuyItem(Item item)
+    {
+        Gold -= item.Gold;
+        Item input_item = new Item(item.Name, item.Atk, item.Def, item.Annot, item.Gold);
+        Inventory.Add(input_item);
+    }
+
+    public void SellItem(int item_index)
+    {
+        Gold += Inventory[item_index].Gold*85/100;
+        Inventory.RemoveAt(item_index);
     }
     public Character(string name, string job, int level, int atk, int def, int hp, int gold)
     {
